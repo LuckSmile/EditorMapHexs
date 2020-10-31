1ea1ac16e7757c92cc3ba170a5db0a8d352819e0
LuckSmile <ahtoh.le04@mail.ru>
2020-10-31 20:04:00 +0300

    New scrips. Map renderings

---
+++ Assets/EditorMapHexs/Scripts/DataEarthHex.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DataEarthHex
+{
+    public Vector2Int Index { get; private set; }
+    public List<DataEarthHex> Childs { get; private set; }
+    public DataEarthHex(Vector2Int index)
+    {
+        this.Index = index;
+        this.Childs = new List<DataEarthHex>();
+    }
+}
---
+++ Assets/EditorMapHexs/Scripts/UIHexCell.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEditor;
+
+public class UIHexCell : MonoBehaviour, IPointerDownHandler
+{
+    public TypesStatus TypeStatus { get; private set; } = TypesStatus.Close;
+    private DataEarthHex data = null;
+    private List<UIHexCell> chillds = null;
+    public static UIHexCell Instantiate(UIHexCell prefab, Transform parent, DataEarthHex data)
+    {
+        UIHexCell cell = Instantiate(prefab, parent);
+        cell.data = data;
+        return cell;
+    }
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        switch(this.TypeStatus)
+        {
+            case TypesStatus.CanBeOpened:
+                for(int index = 0; index < chillds.Count; index++)
+                {
+                    UIHexCell uICell = chillds[index];
+                    uICell.TypeStatus = TypesStatus.CanBeOpened;
+                }
+                break;
+        }
+    }
+    public enum TypesStatus
+    {
+        Open,
+        CanBeOpened,
+        Close
+    }
+}
